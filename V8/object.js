1c02f8b3b8e4e3fd975ef86dd828b7632abee624
Ariman <arimanecro@gmail.com>
2024-02-28 13:50:33 +0200

    Update object.js

--- V8/object.js
+++ V8/object.js
@@ -91,13 +91,72 @@ for (let index = 0; index < 100; index++) {
   });
 }
 
-v8.dp(curlyBrace);
+// UPDATE:
 
-repl.start();
+console.log(process.versions.v8); // 11.8.172.17
+
+{
+  var obj = { name: "Jane Doe", age: 39 };
+
+  // ✅ inobject properties: 1
+  // ✅ unused property fields: 0
+  // ✅ instance size: 40
+}
+
+{
+  var obj = {}; // or new Object / Object.create({});
+
+  //  ❗inobject properties: 4
+  //  ❗unused property fields: 4
+  //  ❗instance size: 56
+}
+
+{
+  var obj = { name: "Jane Doe" };
+  obj.age = 39;
+
+  // ✅ #name --> location: in-object
+  // ❗ #age --> location: properties[0]
+  // ❗ unused property fields: 2
+  // ✅ inobject properties: 1
+  // ✅ instance size: 32
+}
+
+{
+  var obj = { name: "", age: 0 };
+  obj.name = "Jane Doe";
+  obj.age = 39;
+
+  // ✅ #name --> location: in-object
+  // ✅ #age --> location: in-object
+  // ✅ unused property fields: 0
+  // ✅ inobject properties: 2
+  // ✅ instance size: 40
+}
+
+{
+  var obj = {};
+
+  for (let index = 0; index < 10; index++) {
+    Object.defineProperty(curlyBrace, `abc${index}`, {
+      value: `abc${index}`,
+    });      
+    }
+
+  // ❗After the fourth index, elements begin to be stored in a structure --> [properties]
+  // ❗instance size: 56
+  // ❗unused property fields: 2
+  // ❗inobject properties: 4
+}
+
+
+// v8.dp(curlyBrace);
 
 /**
  * RECAP:
  * - The best way to create object is: Object.create({})
- * - The best way to dynamically fill object is to use key as string:
- *   Object.defineProperty(arr, `abc${index}`, { value: index });
- */
+   - The situation with objects is exactly the same as with arrays:
+Each time empty slots are allocated, the memory address where the main (non-empty) elements of the object are located changes (read more in recap of array.js)
+After inserting the fourth element into the object, the remaining elements begin to be stored in a structure called - location: properties which is slower than - location: in-object.
+That's why object fields must be declared in advance 👍
+*/
