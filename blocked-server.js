7f06a0abdea53abca42ca9ee93ac004ca03450af
Ariman <arimanecro@gmail.com>
2024-08-20 18:36:07 +0300

    Update blocked-server.js
    Added new examples of blocking and non-blocking functions

--- blocked-server.js
+++ blocked-server.js
@@ -18,3 +18,52 @@ var blockFunc3 = () => queueMicrotask(blockFunc3)
 var nonBlockFunc = () => setImmediate(nonBlockFunc);
 var nonBlockFunc2 = () => setTimeout(nonBlockFunc2);
 var nonBlockFunc3 = () => setInterval(nonBlockFunc3, 100);
+
+// another example
+{
+setInterval(() => console.log(7777), 100);
+process.nextTick(() => console.log("FIRST NEXT-TICK"));
+
+function unBlockFunc() {
+  for (let index = 0; index < 100; index++) {
+    setImmediate(() => {
+      if (index === 25 || index === 50 || index === 90) {
+        process.nextTick(() => console.log(`NEXT TICK: ${index}`));
+      }
+      console.log(index);
+    });
+  }
+}
+function blockFunc() {
+  for (let index = 0; index < 100; index++) {
+    if (index === 25 || index === 50 || index === 90) {
+      process.nextTick(() => console.log(`NEXT TICK: ${index}`));
+    }
+    Promise.resolve(index).then(i => console.log(i));
+  }
+}
+
+/*
+All promises: 0...100
+FIRST NEXT-TICK
+NEXT TICK: 25
+NEXT TICK: 50
+NEXT TICK: 90
+7777...
+*/
+blockFunc();
+
+/*
+FIRST NEXT-TICK
+setImmediate 1...25
+NEXT TICK: 25
+setImmediate 26...50
+NEXT TICK: 50
+setImmediate 50...90
+NEXT TICK: 90
+setImmediate 50...99
+7777...
+*/
+unBlockFunc()
+
+}
